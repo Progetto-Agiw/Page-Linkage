5690e2d412e0a8049a82f6032c70132ca73b1c0d
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2020-12-07 21:58:23 +0100

    Fixes char decoding by using utf8

--- jaccard-similiarity/leaves_wrapper.py
+++ jaccard-similiarity/leaves_wrapper.py
@@ -5,7 +5,7 @@ class LeavesWrapper:
 
 	#add label to parameters
 	def get_all_leaves(self, path):
-		file = open(path, "r")
+		file = open(path, "r", encoding='utf8')
 		page = file.read()
 		file.close()
 		dom_tree = html.fromstring(page)

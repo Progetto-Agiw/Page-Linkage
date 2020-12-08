47f7e882b4f09265549fadfec6ac05950fa2d0c3
IlariaStocchi <ilaria.stocchi@gmail.com>
2020-12-08 15:08:25 +0100

    html stats NBA

--- scraper/BasketballDomain/scraperNBA_pages.py
+++ scraper/BasketballDomain/scraperNBA_pages.py
@@ -1,4 +1,5 @@
 import scrapy
+import time
 from Filereader import Filereader
 
 
@@ -9,14 +10,21 @@ class NbaSpider(scrapy.Spider):
         
         filereader = Filereader()
         urls = filereader.get_page("url_nba.txt")
-        
-        
+        #urls = ['https://www.nba.com/player/1628035/alfonzo-mckinnie/','https://www.nba.com/player/1630173/precious-achiuwa/']
+        new_urls = []
         for url in urls:
+            url = url[ : (url.rfind('/')-1)]
+            new_url = url[ :(url.rfind('.com/')+5)] + 'stats/' + url[(url.rfind('.com/')+5):(url.rfind('/'))] + '/career'
+            new_urls.append(new_url)
+                
+        for url in new_urls:
+            time.sleep(0.5)
             yield scrapy.Request(url=url, callback=self.parse)
     
 
     def parse(self, response):
-       filename = response.url.split("/")[-1] + '.html'
+       #filename = response.url.split("/")[-1] + '.html'
+       filename = response.url[33:(response.url.rfind('/'))] + '.html'
        with open(filename, 'wb') as f:
            f.write(response.body)
        

4f9be19b14a6cd7adac19e2dfb2587173079ed39
IlariaStocchi <ilaria.stocchi@gmail.com>
2020-12-08 20:37:02 +0100

    Allineate con i nomi anche le pagine di NBA

--- scraper/BasketballDomain/scraperNBA_pages.py
+++ scraper/BasketballDomain/scraperNBA_pages.py
@@ -1,10 +1,12 @@
 import scrapy
 import time
 from Filereader import Filereader
+from collections import deque
 
 
 class NbaSpider(scrapy.Spider):
     name = "bskNBA_players"
+    nomi = deque([])
 
     def start_requests(self):
         
@@ -13,6 +15,8 @@ class NbaSpider(scrapy.Spider):
         #urls = ['https://www.nba.com/player/1628035/alfonzo-mckinnie/','https://www.nba.com/player/1630173/precious-achiuwa/']
         new_urls = []
         for url in urls:
+            temp = (url[url.rfind('https://www.nba.com/player/'):(url.rfind('/'))])
+            NbaSpider.nomi.append(temp[(temp.rfind('/')+1):])
             url = url[ : (url.rfind('/')-1)]
             new_url = url[ :(url.rfind('.com/')+5)] + 'stats/' + url[(url.rfind('.com/')+5):(url.rfind('/'))] + '/career'
             new_urls.append(new_url)
@@ -24,11 +28,15 @@ class NbaSpider(scrapy.Spider):
 
     def parse(self, response):
        #filename = response.url.split("/")[-1] + '.html'
-       filename = response.url[33:(response.url.rfind('/'))] + '.html'
+       #filename = response.url[33:(response.url.rfind('/'))] + '.html'
+       filename = NbaSpider.nomi.popleft() + '.html'
        with open(filename, 'wb') as f:
            f.write(response.body)
        
 
            
+
+
+
         
         

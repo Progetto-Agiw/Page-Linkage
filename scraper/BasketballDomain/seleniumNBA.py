3faf0d9cc8e0b98cdd97b331c0d8d833c780c118
IlariaStocchi <ilaria.stocchi@gmail.com>
2020-12-09 12:58:25 +0100

    selenium for NBA pages

--- scraper/BasketballDomain/seleniumNBA.py
+++ scraper/BasketballDomain/seleniumNBA.py
@@ -1,22 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-
+from Filereader import Filereader
+from collections import deque
 
 GECKO_PATH = "./geckodriver"
 ADBLOCK_PATH = "./adblock_plus-3.10-an fx.xpi"
-
-
-rotoworld_file = open("url_rotoworld.txt", "r")
-content_rotoworld = rotoworld_file.read()
-content_list_rotoworld = content_rotoworld.split(",")
-
-s = content_list_rotoworld
-
 
 ffprofile = webdriver.FirefoxProfile()
 ffprofile.add_extension(ADBLOCK_PATH)
@@ -24,14 +12,31 @@ ffprofile.set_preference("extensions.adblockplus.currentVersion", "3.10")
 binary = FirefoxBinary('/usr/bin/firefox')
 firefox = webdriver.Firefox(firefox_profile=ffprofile, firefox_binary=binary, executable_path=GECKO_PATH)
 
-for page in s:
+filereader = Filereader()
+urls = filereader.get_page("url_nba.txt") 
+
+names = deque([])   
+
+new_urls = []
+for url in urls:
+    
+    temp = (url[url.rfind('https://www.nba.com/player/'):(url.rfind('/'))])
+    names.append(temp[(temp.rfind('/')+1):])
+    
+    url_temp = url[:url.rfind('/')-1]
+    url_id = url_temp[27 :url_temp.rfind('/')]
+    url_finale = 'https://www.nba.com/' + '/stats/player/' + url_id + '/career'
+    new_urls.append(url_finale)
+
+for page in new_urls:
     if(page == ''):
         break
     print('Caricando pagina' + page + '\n')
     firefox.get(page)
     html = firefox.page_source
-    filename = page[page.rfind('/')+1: ] + ".html"
+    filename = names.popleft() + '.html'
     my_file = open(filename, "w")
     print('Scrivendo pagina' + page + '\n')
     my_file.write(str(html))
     my_file.close
+

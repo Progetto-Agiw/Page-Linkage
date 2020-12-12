43dcb4b22c96a1aecca2812212f12f96b4a03fcf
Antonio Turco <truock@gmail.com>
2020-12-12 16:37:16 +0100

    :construction: restituisce recall e precision in base alla grandezza dell'input

--- tf_idf.py
+++ tf_idf.py
@@ -17,11 +17,23 @@ def entity_linkage(dataset_folder, site_a, site_b, max_page = None):
 
 	coppie1 = algoritmo1.faiLaMagia(dict_first_source,dict_second_source)
 
+	i = 1
+	precisions = []
+	recalls = []
+
 	num = 0
 	den = len(coppie1)
+
+	ground_truth_size = max_page if max_page != None else 75
+
 	for coppia in coppie1[:75]:
 	    if(coppia[0] == coppia[1]):
 	        num += 1
+	    current_precision = num / i
+	    current_recall = num / ground_truth_size
+	    precisions.append(current_precision)
+	    recalls.append(current_recall)
+	    i += 1
 
 	precision = num/den
 	print("\n\n La precision del sistema rumoroso è " + str(precision) + "\n\n")
@@ -32,10 +44,12 @@ def entity_linkage(dataset_folder, site_a, site_b, max_page = None):
 	f_measure = 2*(recall*precision)/(recall+precision)
 	print("La f1 measure del sistema rumoroso è " + str(f_measure) + "\n\n")
 
-
+	print(precisions)
+	print(recalls)
 	######## Stampa per la seconda sorgente #########
 	#print("Inizio stampa dei dizionari relativi a tutte le pagine\n\n")
 	#for i in range(0,len(dict_first_source)):
 	#    print("Dizionario n°"+ str(i) +" della seconda sorgente\n")
 	#    print(dict_first_source[i])
 	#    print("\n\n")
+	return precisions, recalls
